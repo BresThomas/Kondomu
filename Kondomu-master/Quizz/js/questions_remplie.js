50bebb6d53f900032b92bec3d78274c9721adb05
Akira98000 <75495075+Akira98000@users.noreply.github.com>
2022-12-02 07:36:55 +0100

    Add files via upload

---
+++ Kondomu-master/Quizz/js/questions_remplie.js
@@ -0,0 +1,151 @@
+let questions = [
+  {
+    numb: 1,
+    question: "MST : QU'EST CE QUE ÇA VEUT DIRE?",
+    answer: "Maladie Sexuellement Transmissible",
+    options: [
+      "Maladie Sexuelle Terrible",
+      "Maladie Sans Traitement",
+      "Maladie Sexuellement Transmissible",
+      "Maladie Sexuelle Traitable"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Combien de personnes par jour contractent une infection sexuellement transmissible dans le monde ? ",
+    answer: "1 million",
+    options: [
+      "100 millions",
+      "1 million",
+      "50 millions",
+      "500 mille"
+    ]
+  },
+    {
+      numb: 3,
+      question: "Le quel de ces moyens de contraception est le seul à protéger des ist ?",
+      answer: "Le préservatif masculin/feminin",
+      options: [
+        "Le retrait",
+        "Le préservatif masculin/feminin",
+        "L'implant",
+        "Le gel hydroalcoolique"
+      ]
+    },
+  
+  
+    {
+      numb: 4,
+      question: "En cas d’absence de traitement, la majorité des personnes porteuses du VIH développent les signes du sida après combien de temps ?",
+    answer: "8 à 10 ans",
+    options: [
+      "8 mois",
+      "3 semaine",
+      "5 ans",
+      "8 à 10 ans"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Laquelle de ces maladies est sexuellement transmissible ?",
+    answer: "L'hépatite B",
+    options: [
+      "l'hépatite C",
+      "L'hépatite B",
+      "l'hépatite E",
+      "l'hépatite Z"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Quels sont le symptôme signe principal d'une infection au VIH ?",
+    answer: "Fievre",
+    options: [
+      "Fievre",
+      "Amaigrissement",
+      "Toux",
+      "Irritation de la peau"
+    ]
+  },
+  {
+    numb: 6,
+    question: "En quelle année s’est développée la pandémie faisant de cette maladie un problème sanitaire mondial ? ",
+    answer: "1970",
+    options: [
+      "1950",
+      "1965",
+      "1992",
+      "1970"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Quelle est la meilleure solution de ce protéger du VIH ?",
+    answer: "Le préservatif",
+    options: [
+      "Le Paracétamol",
+      "Quitter la sociétée",
+      "Le préservatif",
+      "Faire un backflip avant ces rapports sexuels"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Dans les pays en développement, combien de patients bénéficient aujourd’hui d’aucun traitement efficace ? ",
+    answer: "Plus de 95%",
+    options: [
+      "Plus de 95%",
+      "50%",
+      "Moins de 75%",
+      "75%"
+    ]
+  },
+  {
+    numb: 9,
+    question: "La lutte contre le sida est la priorité de quelle(s) association(s) ?",
+    answer: "Sidaction",
+    options: [
+      "ONU",
+      "Sidaction",
+      "GoodPlanet",
+      "SMAVD"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Quelle est la date de la journée mondiale de lutte contre le Sida ? ",
+    answer: "1er decembre",
+    options: [
+      "11 semptembre",
+      "1er decembre",
+      "2 decembre",
+      "3 decembre"
+    ]
+  },
+
+  
+  {
+    numb: 11,
+    question: "En France, combien de personnes vivent avec le VIH ?  ",
+    answer: "173000",
+    options: [
+      "69 420",
+      "173000",
+      "27",
+      "300 000"
+    ]
+  },
+  
+  
+  {
+    numb: 12,
+    question: "Le sida détruit :",
+    answer: "le système immunitaire",
+    options: [
+      "rien",
+      "le pousse des cheveux ",
+      "le système de reproduction",
+      "le système immunitaire"
+    ]
+  },
+];
